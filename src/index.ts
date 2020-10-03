196594de374e98373657a46b46cc74afb627874a
Aadarsh <aadarsh-s@outlook.com>
2020-10-03 17:04:41 +0530

    adding morgan

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import prodRouter from './routes/products';
 import orderRouter from './routes/order';
+import morgan from 'morgan';
 const app = express();
 
+app.use(morgan('dev'));
 app.use('/product', prodRouter);
 app.use('/orders', orderRouter);
 
